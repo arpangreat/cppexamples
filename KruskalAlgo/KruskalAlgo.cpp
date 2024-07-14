b0a9a80ee46844c186a21ee5474da934180f9f6e
arpangreat <arpanthegreat41@gmail.com>
2024-07-14 15:44:18 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- KruskalAlgo/KruskalAlgo.cpp
+++ KruskalAlgo/KruskalAlgo.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-using namespace std;
+
 class Edge {
 public:
   int source;
@@ -17,7 +17,7 @@ int findParent(int v, int *parent) {
 }
 void Kruskal(Edge *input, int n, int E) {
   // Sort the input array - accending order based on weights
-  sort(input, input + E, compare);
+  std::sort(input, input + E, compare);
 
   Edge *output = new Edge[n - 1];
   int *parent = new int[n];
@@ -43,26 +43,24 @@ void Kruskal(Edge *input, int n, int E) {
     i++;
   }
   for (int i = 0; i < n - 1; i++) {
-    cout << endl;
+    std::cout << std::endl;
     if (output[i].source < output[i].dest) {
-      cout << output[i].source << " " << output[i].dest << " "
-           << output[i].weight << endl;
-    }
-
-    else {
-      cout << output[i].dest << " " << output[i].source << " "
-           << output[i].weight << endl;
+      std::cout << output[i].source << " " << output[i].dest << " "
+                << output[i].weight << std::endl;
+    } else {
+      std::cout << output[i].dest << " " << output[i].source << " "
+                << output[i].weight << std::endl;
     }
   }
 }
 int main() {
   int n, E;
-  cin >> n >> E;
+  std::cin >> n >> E;
   Edge *input = new Edge[E];
 
   for (int i = 0; i < E; i++) {
     int s, d, w;
-    cin >> s >> d >> w;
+    std::cin >> s >> d >> w;
     input[i].source = s;
     input[i].dest = d;
     input[i].weight = w;
--- fmtExs/printer.cpp
+++ fmtExs/printer.cpp
@@ -1,7 +1,10 @@
 #include <fmt/color.h>
+#include <fmt/ranges.h>
+#include <vector>
 
 int main(int argc, char *argv[]) {
-  fmt::print(fmt::emphasis::bold | fg(fmt::terminal_color::magenta),
-             "Hello World");
+  std::vector<int> val = {1, 2, 3};
+  fmt::print(fmt::emphasis::bold | fg(fmt::terminal_color::magenta), "{}\n",
+             val);
   return 0;
 }
